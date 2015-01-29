93c6d6f4a4293fc4105de521475d42bd87ddb174
Oscar Robinson <oscarrobinson@btinternet.com>
2015-01-29 16:07:07 +0000

    added timestring queries

--- mainrest.py
+++ mainrest.py
@@ -3,12 +3,14 @@
 # -*- encoding:utf8 -*-
 
 import json
-from flask import Flask, request
+from flask import Flask, request, abort, make_response
 from pymongo import MongoClient
 from bson import json_util
 from flask_cors import CORS
 import time
 import os
+import datetime
+import time
 
 # Handling ObjectId: http://stackoverflow.com/questions/8409194/unable-to-deserialize-pymongo-objectid-from-json
 # Handling spaces in URL: replace ' ' character with '%20', e.g. "Hello World" -> "Hello%20World"
@@ -22,6 +24,8 @@ cors = CORS(app)
 client = MongoClient("127.0.0.1", 27017)
 db = client.tempdata
 readingsCollection = db.temps
+
+
 
 #-----------------------------#
 #       ERROR HANDLERS        #
@@ -46,19 +50,7 @@ def jsonListAppend(jList, listItem, comma=True):
         return jList[:-2]+(json.dumps(listItem, sort_keys=True, indent=4, separators=(',', ': '), default=json_util.default))+"]}"
 
 
-#-----------------------------#
-#      ROUTING FUNCTIONS      #
-#-----------------------------#
-
-#Used to get readings in certain time range
-@app.route('/api/readings', methods=['GET'])
-def readings():
-    startTime = request.args.get('start', '')
-    endTime = request.args.get('end', '')
-    if not startTime:
-        startTime = time.time()-1440
-    if not endTime:
-        endTime = time.time()
+def getReadings(startTime, endTime):
     result = createJsonList("readings")
     allReadings = readingsCollection.find({"time": {"$gt": str(startTime), "$lt": str(endTime)}})
     count = 0
@@ -69,6 +61,45 @@ def readings():
             result = jsonListAppend(result, reading)
         count+=1
     return result
+
+#-----------------------------#
+#      ROUTING FUNCTIONS      #
+#-----------------------------#
+
+#Used to get readings in certain time range
+@app.route('/api/readings-unixtime', methods=['GET'])
+def readingsUnixTime():
+    startTime = request.args.get('start', '')
+    endTime = request.args.get('end', '')
+    if not startTime:
+        startTime = time.time()-86400
+    if not endTime:
+        endTime = time.time()
+    return getReadings(startTime, endTime)
+
+@app.route('/api/readings', methods=['GET'])
+def readings():
+    #1-12-1994-12:12
+    startTime = request.args.get('start', '')
+    endTime = request.args.get('end', '')
+    try:
+        if startTime:
+            startDateTime =  time.strptime(startTime, "%d-%m-%Y-%H:%M")
+        else:
+            startDateTime = time.strptime(time.ctime(time.time()-86400))
+        if endTime:
+            endDateTime = time.strptime(endTime, "%d-%m-%Y-%H:%M")
+        else:
+            endDateTime = time.strptime(time.ctime())
+    except Exception:
+        abort(400)
+
+    print time.asctime(startDateTime)
+    print time.asctime(endDateTime)
+
+    return getReadings(time.mktime(startDateTime), time.mktime(endDateTime))
+
+
 
 #-----------------------------#
 #           INIT API          #
